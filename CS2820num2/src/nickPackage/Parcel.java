94e6f226e933e15a4a8cf2e19ecd6b2bc6ccb9c6
nicholasbarnes <nicholasbarnes@wireless-nat-inside.uiowa.edu>
2016-12-07 09:36:54 -0600

    Added Parcel

---
+++ CS2820num2/src/nickPackage/Parcel.java
@@ -0,0 +1,29 @@
+package nickPackage;
+
+import src.Item;
+import src.String;
+
+public class Parcel {
+  /**
+   * 
+   * @author Ted Herman, Nicholas Barnes
+   * 
+   * A Parcel has an address and a list of Items
+   *
+   */
+	
+  String address;
+ // Item[] items;
+  boolean finished;
+  public Parcel(String address, Item item){
+	  this.address = address;
+	  finished = false;
+  }
+  public Parcel(String address, Item[] items) { 
+	this.address = address;
+//	this.items = items;
+	finished = false;
+    }
+  public boolean isFinished() { return finished; }
+  public void setFinished() { finished = true; }
+  }
