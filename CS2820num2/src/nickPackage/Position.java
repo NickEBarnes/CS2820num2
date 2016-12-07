ef166de4480be5c1182715ff93300964d5ad17e0
nicholasbarnes <nicholasbarnes@wireless-nat-inside.uiowa.edu>
2016-12-07 09:42:02 -0600

    Added Position

---
+++ CS2820num2/src/nickPackage/Position.java
@@ -0,0 +1,25 @@
+package nickPackage;
+
+/**
+ * 
+ * @author Wei Gui
+ *
+ */
+public class Position {
+	private int x;
+	private int y;
+	public Position(int a, int b){
+		this.x=a;
+		this.y=b;
+	}
+	public int getx() {
+		return x;
+	}
+	public int gety() {
+		return y;
+	}
+	public void set(int a,int b) {
+		this.x=a;
+		this.y=b;
+	}
+}
