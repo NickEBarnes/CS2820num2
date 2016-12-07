11de6a9f46775b500420820502f649d82081ca4d
nicholasbarnes <nicholasbarnes@wireless-nat-inside.uiowa.edu>
2016-12-06 22:44:59 -0600

    Added Cell

---
+++ CS2820num2/src/nickPackage/Cell.java
@@ -0,0 +1,63 @@
+package nickPackage;
+
+
+//import src.Integer;
+//import src.MockFloor;
+//import src.Parcel;
+//import src.Point;
+//import src.String;
+
+//Nicholas Barnes
+
+//import warehouse_system.*;
+//import warehouse_system.belt.Parcel;
+//import warehouse_system.floor.*;
+
+public class Cell  {
+ Integer cellNum = 0;
+ Point p2;
+ Point p1;
+ Parcel pOrig;
+ MockFloor m;
+ MockFloor RetM;
+ Point cellHas;
+ Integer cellX;
+ Integer cellY;
+ Object o;
+ 
+ public Cell(){
+	   
+ }
+ 
+ public Object getContents(){
+	   return 1;
+ }
+ public void getCell(Point one){
+	    System.out.println(one.getName());
+	    
+ }
+ public Object cellType(Point p1){
+	   System.out.println(p1.getObject().toString());
+	   return p1.getObject();
+	   
+ }
+ public boolean isBin(Point p1){
+	   Point myP = p1;
+	   String name = p1.getName();
+	   if (name == "Bin"){
+		   System.out.println("At belt spot- x: " + myP.getX() + " and y: " + myP.getY() + " is object type: " + name);
+		   return true;
+	   }
+	   else {
+		   System.out.println(" WTF At belt spot- x: " + myP.getX() + " and y: " + myP.getY() + " is object type: " + name);
+		   return false;
+	   }
+ }
+ 
+ public Parcel setContents(Parcel p){
+	   //will replace bin with parcel
+	   pOrig = p;
+	   return pOrig;
+ }
+ 
+}
