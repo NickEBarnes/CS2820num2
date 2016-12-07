19628dd145f43e86e65b9866e8119f0e6afec539
nicholasbarnes <nicholasbarnes@wireless-nat-inside.uiowa.edu>
2016-12-07 09:54:49 -0600

    Added wareHouseLocation

---
+++ CS2820num2/src/nickPackage/wareHouseLocation.java
@@ -0,0 +1,26 @@
+package nickPackage;
+
+//import src.Point;
+
+/**
+ * 
+ * @author josephtleiferman
+ *
+ */
+
+public abstract class wareHouseLocation {
+	private Point location; 
+	
+	public wareHouseLocation(Point p) {
+		location = p;
+	}
+	public Point getLocation() {
+		return location;
+	}
+	public void setLocatoin(int x, int y) {
+		location.setPoint(x,y);
+	}
+	
+
+}
+
