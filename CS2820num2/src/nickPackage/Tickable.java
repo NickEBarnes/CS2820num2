550547fac2491574fff42f80bc417d992486c008
nicholasbarnes <nicholasbarnes@wireless-nat-inside.uiowa.edu>
2016-12-07 09:51:54 -0600

    Added Tickable

---
+++ CS2820num2/src/nickPackage/Tickable.java
@@ -0,0 +1,16 @@
+package nickPackage;
+
+
+/**
+ * 
+ * @author Di Huang
+ *
+ */
+public interface Tickable {
+	/**
+	 * This interface works as a tunnel that transmits ticks 
+	 * from central clock to other components
+	 */
+	public void tick(int tick);
+	
+}
