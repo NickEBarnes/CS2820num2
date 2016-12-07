e295394c48948b7ba8ddd68b0aca0af6bbac27d5
nicholasbarnes <nicholasbarnes@wireless-nat-inside.uiowa.edu>
2016-12-07 09:39:11 -0600

    Added Picker

---
+++ CS2820num2/src/nickPackage/Picker.java
@@ -0,0 +1,19 @@
+package nickPackage;
+
+//import src.Robot;
+//import src.String;
+
+/**
+ * 
+ * @author Ted Herman
+ * 
+ * The Picker interface is implemented by the Orders
+ * component; when a Robot arrives to the Picker, it 
+ * invokes Picker.notify() to tell the Orders that the
+ * requested Robot has arrived with a shelf
+ * 
+ */
+public interface Picker {
+  void notify(Robot r, String itemShelf);
+ 
+}
